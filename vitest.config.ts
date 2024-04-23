cc4080448c31f38f6820b29cbf421b29fe2cd8aa
forlorn <clickshaq11@gmail.com>
2024-04-23 16:47:31 +0300

    add setupfiles for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      include: [
-        'src/**/*.test.ts',
-        'src/**/*.test.tsx',
-      ],
+      include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
       globals: true,
-      environment: 'jsdom',
-      reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default']
-    }
+      environment: "jsdom",
+      reporters: process.env.GITHUB_ACTIONS
+        ? ["default", "github-actions"]
+        : ["default"],
+      setupFiles: ["src/__tests__/setup/server.ts"],
+    },
   })
 );
